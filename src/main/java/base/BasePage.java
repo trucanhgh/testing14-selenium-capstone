767e5fff5b31664d5ebe2c9bfa7dc1b5aa32b910
trucanhgh <22520082@gm.uit.edu.vn>
2026-04-27 12:20:59 +0700

    rename DeleteCourse.java to DeleteCourseTest.java and update Gradle and project settings

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -4,11 +4,16 @@ import constants.TimeOutConstant;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class BasePage {
     protected final Logger LOG = LogManager.getLogger(getClass());
     protected WebDriver driver;
@@ -17,42 +22,225 @@ public class BasePage {
         this.driver = driver;
     }
 
-    public WebElement waitForElementVisible(By locator, long timeOutInSec) {
+    public WebElement waitForVisible(By locator, long timeOutInSec) {
         WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    public WebElement waitForElementClickable(By locator, long timeOutInSec) {
+    public WebElement waitForClickable(By locator, long timeOutInSec) {
         WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
         return wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
 
-    public void inputText(By locator, String value, long timeOutInSec) {
-        WebElement element = waitForElementVisible(locator, timeOutInSec);
-        LOG.info("Input value: {} into locator: {}", value, locator);
-        element.sendKeys(value);
+    public WebElement waitForPresent(By locator, long timeOutInSec) {
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
+        return wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
-    public void inputText(By locator, String value) {
-        inputText(locator, value, TimeOutConstant.TIME_OUT_DEFAULT);
+    public boolean waitForInvisible(By locator, long timeOutInSec) {
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
     }
 
-    public void clickBtn(By locator, long timeOutInSec) {
+    public void click(By locator, long timeOutInSec) {
         LOG.info("Click locator: {}", locator);
-        WebElement element = waitForElementClickable(locator, timeOutInSec);
+        WebElement element = waitForClickable(locator, timeOutInSec);
         element.click();
     }
 
-    public void clickBtn(By locator) {
-        clickBtn(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    public void click(By locator) {
+        click(locator, TimeOutConstant.TIME_OUT_DEFAULT);
     }
 
-    public String getTextElement(By locator, long timeOutInSec) {
-        WebElement element = waitForElementVisible(locator, timeOutInSec);
+    public void type(By locator, String value, long timeOutInSec) {
+        WebElement element = waitForVisible(locator, timeOutInSec);
+        LOG.info("Type value: {} into locator: {}", value, locator);
+        element.clear();
+        element.sendKeys(value);
+    }
+
+    public void type(By locator, String value) {
+        type(locator, value, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public void clear(By locator, long timeOutInSec) {
+        WebElement element = waitForVisible(locator, timeOutInSec);
+        LOG.info("Clear locator: {}", locator);
+        element.clear();
+    }
+
+    public void clear(By locator) {
+        clear(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public void clearAndType(By locator, String value, long timeOutInSec) {
+        WebElement element = waitForVisible(locator, timeOutInSec);
+        LOG.info("Clear and type value: {} into locator: {}", value, locator);
+        element.clear();
+        element.sendKeys(value);
+    }
+
+    public void clearAndType(By locator, String value) {
+        clearAndType(locator, value, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public String getText(By locator, long timeOutInSec) {
+        WebElement element = waitForVisible(locator, timeOutInSec);
         return element.getText();
     }
 
-    public String getTextElement(By locator) {
-        return getTextElement(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    public String getText(By locator) {
+        return getText(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public boolean isDisplayed(By locator, long timeOutInSec) {
+        try {
+            WebElement element = waitForVisible(locator, timeOutInSec);
+            return element.isDisplayed();
+        } catch (Exception e) {
+            LOG.warn("Element not found or not visible for locator: {}", locator);
+            return false;
+        }
+    }
+
+    public boolean isDisplayed(By locator) {
+        return isDisplayed(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public boolean isPresent(By locator, long timeOutInSec) {
+        try {
+            waitForPresent(locator, timeOutInSec);
+            return true;
+        } catch (Exception e) {
+            LOG.warn("Element not found for locator: {}", locator);
+            return false;
+        }
+    }
+
+    public boolean isPresent(By locator) {
+        return isPresent(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public List<WebElement> getElements(By locator) {
+        return driver.findElements(locator);
+    }
+
+    public int getElementCount(By locator) {
+        return getElements(locator).size();
+    }
+
+    public void scrollToElement(By locator, long timeOutInSec) {
+        WebElement element = waitForPresent(locator, timeOutInSec);
+        LOG.info("Scroll to element locator: {}", locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", element);
+    }
+
+    public void scrollToElement(By locator) {
+        scrollToElement(locator, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public void scrollToTop() {
+        LOG.info("Scroll to top");
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0);");
+    }
+
+    public void scrollToBottom() {
+        LOG.info("Scroll to bottom");
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
+
+    public void selectByVisibleText(By locator, String text, long timeOutInSec) {
+        WebElement element = waitForVisible(locator, timeOutInSec);
+        LOG.info("Select text: {} from locator: {}", text, locator);
+        new Select(element).selectByVisibleText(text);
+    }
+
+    public void selectByVisibleText(By locator, String text) {
+        selectByVisibleText(locator, text, TimeOutConstant.TIME_OUT_DEFAULT);
+    }
+
+    public void inputText(By locator, String value, long timeOutInSec) {
+        type(locator, value, timeOutInSec);
+    }
+
+    public void inputText(By locator, String value) {
+        type(locator, value);
+    }
+
+    public void clickBtn(By locator, long timeOutInSec) {
+        click(locator, timeOutInSec);
+    }
+
+    public void clickBtn(By locator) {
+        click(locator);
+    }
+
+    public WebElement waitForElementVisible(By locator, long timeOutInSec) {
+        return waitForVisible(locator, timeOutInSec);
+    }
+
+    public WebElement waitForElementClickable(By locator, long timeOutInSec) {
+        return waitForClickable(locator, timeOutInSec);
+    }
+
+    public String getElementText(By locator, long timeOutInSec) {
+        return getText(locator, timeOutInSec);
+    }
+
+    public String getElementText(By locator) {
+        return getText(locator);
+    }
+
+    public boolean isElementDispayed(By locator, long timeOutInSec) {
+        return isDisplayed(locator, timeOutInSec);
+    }
+
+    public boolean isElementDisplayed(By locator, long timeOutInSec) {
+        return isDisplayed(locator, timeOutInSec);
+    }
+
+    public boolean isElementDisplayed(By locator) {
+        return isDisplayed(locator);
+    }
+
+    public void refreshPage() {
+        LOG.info("Refresh page");
+        driver.navigate().refresh();
+    }
+
+    public void goBack() {
+        LOG.info("Navigate back");
+        driver.navigate().back();
+    }
+
+    public void goForward() {
+        LOG.info("Navigate forward");
+        driver.navigate().forward();
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    public boolean waitForUrlContains(String partialUrl, long timeOutInSec) {
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
+        return wait.until(ExpectedConditions.urlContains(partialUrl));
+    }
+
+    public boolean waitForTitleContains(String partialTitle, long timeOutInSec) {
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
+        return wait.until(ExpectedConditions.titleContains(partialTitle));
+    }
+
+    public boolean waitForPageReady(long timeOutInSec) {
+        WebDriverWait wait = new WebDriverWait(driver, java.time.Duration.ofSeconds(timeOutInSec));
+        return wait.until(webDriver -> {
+            Object readyState = ((JavascriptExecutor) webDriver).executeScript("return document.readyState");
+            return "complete".equals(readyState);
+        });
     }
 }
--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -7,6 +7,7 @@ import report.ExtentReportManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -14,57 +15,126 @@ import java.lang.reflect.Method;
 
 
 public class BaseTest {
+    private static final String DEFAULT_BASE_URL = "https://demo2.cybersoft.edu.vn";
     protected final Logger LOG = LogManager.getLogger(getClass());
 
     @BeforeSuite
-    public void beforeSuite() {
-        LOG.info("Starting berfore suite - initialize extent report");
+    public void setUpSuite() {
+        LOG.info("Start suite setup - initialize extent report");
         ExtentReportManager.initializeExtentReports();
     }
 
     @BeforeClass
     @Parameters({"browser"})
-    public void beforeClass(@Optional String browserFromSuite) {
+    public void setUpClass(@Optional String browserFromSuite) {
         String browser = resolveBrowser(browserFromSuite);
-        LOG.info("Starting before class - initialize web driver for browser: {}", browser);
+        LOG.info("Start class setup - initialize web driver for browser: {}", browser);
 
         DriverManager driverManager = DriverManagerFactory.getDriverManager(browser);
         driverManager.createWebDriver();
 
         WebDriver driver = driverManager.getDriver();
+        driver.manage().window().maximize();
         DriverFactory.setDriver(driver);
     }
 
     @BeforeMethod
-    public void beforeMethod(Method method) {
-        LOG.info("Starting before method - create test in extent report for method: " + method.getName());
+    public void setUpMethod(Method method) {
+        LOG.info("Start test method - create extent test for method: {}", method.getName());
         ExtentReportManager.createTest(method.getName());
     }
 
     @AfterMethod
-    public void afterMethod(ITestResult testResult) {
-        LOG.info("Starting after method - capture screenshot if test failed for method: " + testResult.getName());
+    public void tearDownMethod(ITestResult testResult) {
+        LOG.info("End test method - handle result for method: {}", testResult.getName());
         if (testResult.getStatus() == ITestResult.FAILURE) {
             String testName = testResult.getName() != null ? testResult.getName() : testResult.getMethod().getMethodName();
             ExtentReportManager.captureScreenshot(DriverFactory.getDriver(), testName);
-            ExtentReportManager.fail(testResult.getThrowable().toString());
+            Throwable throwable = testResult.getThrowable();
+            ExtentReportManager.fail(throwable != null ? throwable.toString() : "Test failed without throwable");
         }
     }
 
     @AfterClass
-    public void afterClass() {
-        LOG.info("Starting after class - quit web driver");
+    public void tearDownClass() {
+        LOG.info("End class setup - quit web driver");
         WebDriver driver = DriverFactory.getDriver();
-        if (driver != null) {
-            driver.quit();
+        try {
+            if (driver != null) {
+                driver.quit();
+            }
+        } finally {
+            DriverFactory.removeDriverThreadLocal();
         }
-        DriverFactory.removeDriverThreadLocal();
     }
 
     @AfterSuite
-    public void afterSuite() {
-        LOG.info("Starting after suite - flush extent report");
+    public void tearDownSuite() {
+        LOG.info("End suite setup - flush extent report");
         ExtentReportManager.flushReports();
+    }
+
+    protected WebDriver getDriver() {
+        return DriverFactory.getDriver();
+    }
+
+    protected String getBaseUrl() {
+        String baseUrl = System.getProperty("baseUrl");
+        if (baseUrl != null && !baseUrl.trim().isEmpty()) {
+            return baseUrl.trim();
+        }
+        return DEFAULT_BASE_URL;
+    }
+
+    protected void openBaseUrl() {
+        openUrl(getBaseUrl());
+    }
+
+    protected void openUrl(String urlOrPath) {
+        WebDriver driver = getDriver();
+        String targetUrl = urlOrPath;
+
+        if (urlOrPath != null && !urlOrPath.trim().isEmpty() && !urlOrPath.startsWith("http://") && !urlOrPath.startsWith("https://")) {
+            String baseUrl = getBaseUrl();
+            if (urlOrPath.startsWith("/")) {
+                targetUrl = baseUrl + urlOrPath;
+            } else {
+                targetUrl = baseUrl + "/" + urlOrPath;
+            }
+        }
+
+        LOG.info("Open URL: {}", targetUrl);
+        driver.get(targetUrl);
+    }
+
+    protected void maximizeWindow() {
+        WebDriver driver = getDriver();
+        if (driver != null) {
+            driver.manage().window().maximize();
+        }
+    }
+
+    protected void refreshPage() {
+        WebDriver driver = getDriver();
+        if (driver != null) {
+            driver.navigate().refresh();
+        }
+    }
+
+    protected void waitForPageReady() {
+        WebDriver driver = getDriver();
+        if (driver == null) {
+            return;
+        }
+
+        long timeoutSeconds = 20;
+        long started = System.currentTimeMillis();
+        while (System.currentTimeMillis() - started < timeoutSeconds * 1000L) {
+            Object readyState = ((JavascriptExecutor) driver).executeScript("return document.readyState");
+            if ("complete".equals(readyState)) {
+                return;
+            }
+        }
     }
 
     private String resolveBrowser(String browserFromSuite) {
